8c9c85e51a2f6c4aa236c37ef5f501226df43079
evilbutcher <pgcrfhht@outlook.com>
2021-03-12 07:55:10 +0800

    Update Env.js

--- Env.js
+++ Env.js
@@ -1,3 +1,6 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: brown; icon-glyph: terminal;
 /**
  * Author: GideonSenku
  * Github: https://github.com/GideonSenku
